2a45b34a26729a32c8dc44a96df6ec2b9d443821
Tao Su <96sutao@gmail.com>
2025-05-28 23:47:55 -0400

    Modified the site title

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Tao Su's",
+	title: "Tao Su",
 	subtitle: "Personal Blog",
 	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
