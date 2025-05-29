f7ac3935f3cef45563ff42925aa582c06c4e4022
Tao Su <96sutao@gmail.com>
2025-05-28 22:05:55 -0400

    config astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://tomstao.github.io",
-	base: "/",
+	base: "/fuwari/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
